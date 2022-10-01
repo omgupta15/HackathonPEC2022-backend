9991ff55238a4e046bf6d1fcb25f280fff7b6195
Om Gupta <om.gupta.1507@gmail.com>
2022-10-01 06:27:10 +0530

    Update firebase keys in environment variables.

--- src/app.js
+++ src/app.js
@@ -4,19 +4,7 @@ require("dotenv").config();
 const firebase = require("firebase-admin");
 
 firebase.initializeApp({
-  credential: firebase.credential.cert({
-    type: "service_account",
-    project_id: "hackathonpec-2022-backend",
-    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-    private_key: process.env.FIREBASE_PRIVATE_KEY,
-    client_email: process.env.FIREBASE_CLIENT_EMAIL,
-    client_id: process.env.FIREBASE_CLIENT_ID,
-    auth_uri: "https://accounts.google.com/o/oauth2/auth",
-    token_uri: "https://oauth2.googleapis.com/token",
-    auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
-    client_x509_cert_url:
-      "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-pq381%40hackathonpec-2022-backend.iam.gserviceaccount.com",
-  }),
+  credential: firebase.credential.cert(JSON.parse(process.env.FIREBASE_KEYS)),
 });
 
 // MongoDB
